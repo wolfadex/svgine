069827567936c148e093a35940cdcbdead237667
Wolfgang Schuster <wschuster@shoretel.com>
2017-04-03 15:51:58 -0500

    Build attempt 7

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Heart from './Engine/Heart';
 import View from './Engine/View';
 import * as vector2 from './vector2';
 
-export default {
+export {
 	// svgineActions: gameStateActions,
 	reducer,
 	// Heart,
