70447383fda2a13dc4e359f63d312132f772a7f9
Wolfgang Schuster <wschuster@shoretel.com>
2017-04-03 15:21:06 -0500

    Build attempt 4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import * as gameStateActions from './actions/gameState';
-import { default as reducer } from './reducers/gameState';
-import Heart from './Engine/Heart';
-import View from './Engine/View';
-import * as vector2 from './vector2';
+// import * as gameStateActions from './actions/gameState';
+// import { default as reducer } from './reducers/gameState';
+// import Heart from './Engine/Heart';
+// import View from './Engine/View';
+// import * as vector2 from './vector2';
+//
+// export default {
+// 	// svgineActions: gameStateActions,
+// 	reducer,
+// 	// Heart,
+// 	vector2,
+// 	View,
+// }
+import React from 'react';
 
-export default {
-	// svgineActions: gameStateActions,
-	reducer,
-	// Heart,
-	vector2,
-	View,
-}
+class Carl extends React.Component {
+    render() {
+        return (
+            <div>
+                Carl was here
+            </div>
+        )
+    }
+};
+
+export default Carl;
